6b785754048285cf0c297a15f0463309c5f38482
Marcello Urbani <marcello.urbani@gmail.com>
2019-02-01 09:41:34 +0000

    creation - not completed

--- src/adt/operations/AdtObjectCreator.ts
+++ src/adt/operations/AdtObjectCreator.ts
@@ -1,130 +1,34 @@
-import { Uri, window } from "vscode"
-import { parseToPromise, getNode, recxml2js } from "../parsers/AdtParserBase"
-import { mapWith } from "../../functions"
-import { AdtServer } from "../AdtServer"
-import { isAbapNode, AbapNode } from "../../fs/AbapNode"
 import {
-  selectObjectType,
-  CreatableObjectType,
-  NewObjectConfig,
-  getObjectType,
-  PACKAGE
-} from "./AdtObjectTypes"
-import { selectTransport } from "../AdtTransports"
+  CreatableType,
+  GroupTypeIds,
+  isGroupType,
+  ObjectType
+} from "abap-adt-api"
+import {
+  CreatableTypeIds,
+  NewObjectOptions,
+  NonGroupTypeIds,
+  ValidateOptions
+} from "abap-adt-api/build/api"
+import { CreatableTypes } from "abap-adt-api/build/api"
+import { Uri, window } from "vscode"
+import { AbapNode, isAbapNode } from "../../fs/AbapNode"
 import { abapObjectFromNode } from "../abap/AbapObjectUtilities"
-import { ObjectType } from "abap-adt-api"
-
-interface ValidationMessage {
-  SEVERITY: string
-  SHORT_TEXT: string
-  LONG_TEXT: string
-}
+import { AdtServer } from "../AdtServer"
+import { selectTransport } from "../AdtTransports"
+import { NewObjectConfig, PACKAGE, selectObjectType } from "./AdtObjectTypes"
 
 export class AdtObjectCreator {
   private types?: ObjectType[]
 
   constructor(private server: AdtServer) {}
 
-  async getObjectTypes(uri: Uri): Promise<ObjectType[]> {
+  public async getObjectTypes(uri: Uri): Promise<ObjectType[]> {
     if (!this.types) this.types = await this.server.client.loadTypes()
     const parent = this.server.findNode(uri)
     let otype = parent && isAbapNode(parent) && parent.abapObject.type
     if (otype === PACKAGE) otype = ""
     return this.types!.filter(x => x.PARENT_OBJECT_TYPE === otype)
-  }
-
-  private getHierarchy(uri: Uri | undefined): AbapNode[] {
-    if (uri)
-      try {
-        return this.server.findNodeHierarchy(uri)
-      } catch (e) {}
-    return []
-  }
-
-  private async guessOrSelectObjectType(
-    hierarchy: AbapNode[]
-  ): Promise<CreatableObjectType | undefined> {
-    const base = hierarchy[0]
-    //if I picked the root node,a direct descendent or a package just ask the user to select any object type
-    // if not, for abap nodes pick child objetc types (if any)
-    // for non-abap nodes if it's an object type guess the type from the children
-    if (hierarchy.length > 2)
-      if (isAbapNode(base)) return selectObjectType(base.abapObject.type)
-      else {
-        const child = [...base]
-          .map(c => c[1])
-          .find(c => isAbapNode(c) && c.abapObject.type !== PACKAGE)
-        if (child && isAbapNode(child)) {
-          const guessed = getObjectType(child.abapObject.type)
-          if (guessed) return guessed
-        }
-      }
-    //default...
-    return selectObjectType()
-  }
-
-  private async validateObject(
-    objType: CreatableObjectType,
-    objDetails: NewObjectConfig
-  ): Promise<ValidationMessage[]> {
-    const url = this.server.connection.createUri(
-      objType.getValidatePath(objDetails)
-    )
-    const response = await this.server.connection.request(url, "POST")
-    const rawValidation = await parseToPromise()(response.body)
-    return getNode(
-      "asx:abap/asx:values/DATA",
-      mapWith(recxml2js),
-      rawValidation
-    ) as ValidationMessage[]
-  }
-
-  /**
-   * Finds or ask the user to select/create a transport for an object.
-   * Returns an empty string for local objects
-   *
-   * @param objType Object type
-   * @param objDetails Object name, description,...
-   */
-  private async selectTransport(
-    objType: CreatableObjectType,
-    objDetails: NewObjectConfig
-  ): Promise<string> {
-    return selectTransport(
-      objType.getPath(objDetails),
-      objDetails.devclass,
-      this.server.client
-    )
-  }
-  /**
-   * Creates an ABAP object
-   *
-   * @param objType Object type descriptor
-   * @param objDetails Object details (name, description, package,...)
-   * @param request Transport request
-   */
-  private async create(
-    objType: CreatableObjectType,
-    objDetails: NewObjectConfig,
-    request: string
-  ) {
-    const conn = await this.server.connection.getStatelessClone()
-    const uri = conn
-      .createUri(objType.getBasePath(objDetails))
-      .with({ query: request && `corrNr=${request}` })
-    let body = objType.getCreatePayload(objDetails)
-    let response = await conn.request(uri, "POST", {
-      body,
-      headers: { "Content-Type": "application/*" }
-    })
-    return response
-  }
-  private async askInput(
-    prompt: string,
-    uppercase: boolean = true
-  ): Promise<string> {
-    const res = (await window.showInputBox({ prompt })) || ""
-    return uppercase ? res.toUpperCase() : res
   }
 
   /**
@@ -187,12 +91,110 @@ export class AdtObjectCreator {
     await this.create(objType, objDetails, trnumber) //exceptions will bubble up
     const obj = abapObjectFromNode(objType.objNode(objDetails))
     await obj.loadMetadata(this.server.client)
-    return objType.getPath(objDetails)
+    return (objDetails)
   }
   guessParentByType(hierarchy: AbapNode[], type: string): string {
     //find latest package parent
     const pn = hierarchy.find(n => isAbapNode(n) && n.abapObject.type === type)
     //return package name or blank string
     return (pn && isAbapNode(pn) && pn.abapObject.name) || ""
+  }
+
+  private getHierarchy(uri: Uri | undefined): AbapNode[] {
+    if (uri)
+      try {
+        return this.server.findNodeHierarchy(uri)
+      } catch (e) {}
+    return []
+  }
+
+  private async guessOrSelectObjectType(
+    hierarchy: AbapNode[]
+  ): Promise<CreatableType | undefined> {
+    const base = hierarchy[0]
+    //if I picked the root node,a direct descendent or a package just ask the user to select any object type
+    // if not, for abap nodes pick child objetc types (if any)
+    // for non-abap nodes if it's an object type guess the type from the children
+    if (hierarchy.length > 2)
+      if (isAbapNode(base)) return selectObjectType(base.abapObject.type)
+      else {
+        const child = [...base]
+          .map(c => c[1])
+          .find(c => isAbapNode(c) && c.abapObject.type !== PACKAGE)
+        if (child && isAbapNode(child)) {
+          const typeid = child.abapObject.type as CreatableTypeIds
+          const guessed = CreatableTypes.get(typeid)
+          if (guessed) return guessed
+        }
+      }
+    //default...
+    return selectObjectType()
+  }
+
+  private async validateObject(objDetails: NewObjectOptions) {
+    let validateOptions: ValidateOptions
+    if (isGroupType(objDetails.objtype))
+      validateOptions = {
+        description: objDetails.description,
+        fugrname: objDetails.parentName,
+        objname: objDetails.name,
+        objtype: objDetails.objtype as GroupTypeIds
+      }
+    else
+      validateOptions = {
+        description: objDetails.description,
+        objname: objDetails.name,
+        objtype: objDetails.objtype as NonGroupTypeIds,
+        packagename: objDetails.parentName
+      }
+    return this.server.client.validateNewObject(validateOptions)
+  }
+
+  /**
+   * Finds or ask the user to select/create a transport for an object.
+   * Returns an empty string for local objects
+   *
+   * @param objType Object type
+   * @param objDetails Object name, description,...
+   */
+  private async selectTransport(
+    objType: CreatableObjectType,
+    objDetails: NewObjectConfig
+  ): Promise<string> {
+    return selectTransport(
+      objType.getPath(objDetails.),
+      objDetails.devclass,
+      this.server.client
+    )
+  }
+  /**
+   * Creates an ABAP object
+   *
+   * @param objType Object type descriptor
+   * @param objDetails Object details (name, description, package,...)
+   * @param request Transport request
+   */
+  private async create(
+    objType: CreatableType,
+    objDetails: NewObjectConfig,
+    request: string
+  ) {
+    const conn = await this.server.connection.getStatelessClone()
+    const uri = conn
+      .createUri(objType.getBasePath(objDetails))
+      .with({ query: request && `corrNr=${request}` })
+    let body = objType.getCreatePayload(objDetails)
+    let response = await conn.request(uri, "POST", {
+      body,
+      headers: { "Content-Type": "application/*" }
+    })
+    return response
+  }
+  private async askInput(
+    prompt: string,
+    uppercase: boolean = true
+  ): Promise<string> {
+    const res = (await window.showInputBox({ prompt })) || ""
+    return uppercase ? res.toUpperCase() : res
   }
 }
--- src/adt/operations/AdtObjectTypes.ts
+++ src/adt/operations/AdtObjectTypes.ts
@@ -1,6 +1,9 @@
-import { sapEscape } from "../../functions"
-import { window, QuickPickItem } from "vscode"
-import { ObjectNode } from "../parsers/AdtNodeStructParser"
+import {
+  CreatableType,
+  CreatableTypes,
+  parentTypeId
+} from "abap-adt-api/build/api"
+import { window } from "vscode"
 
 export const PACKAGE = "DEVC/K"
 
@@ -11,179 +14,13 @@ export interface NewObjectConfig {
   devclass: string
   responsible: string
 }
-function expand(template: string, values: any): string {
-  return template.replace(/{([^}]+)}/g, (x, y) => values[y] || "")
-}
 
-export function objMap(fn: (x: any) => any): (o: Object) => Object
-export function objMap(fn: (x: any) => any, orig?: Object): Object {
-  function mapper(or: any) {
-    return Object.keys(or).reduce(
-      (retval, key) => {
-        const mapped = fn(or[key])
-        if (mapped !== undefined) retval[key] = mapped
-        return retval
-      },
-      {} as any
-    )
-  }
-  return orig ? mapper(orig) : mapper
-}
-const escapeObj = objMap(sapEscape)
-
-export class CreatableObjectType implements QuickPickItem {
-  protected _parentType: string
-  get parentType() {
-    return this._parentType
-  }
-  constructor(
-    public readonly type: string,
-    public readonly label: string,
-    public readonly rootName: string,
-    public readonly nameSpace: string,
-    public readonly pathTemplate: string,
-    public readonly validateTemplate: string
-  ) {
-    this._parentType = PACKAGE
-  }
-  protected escapedValues(config: NewObjectConfig): any {
-    return escapeObj({
-      ...config,
-      type: this.type,
-      parentType: this._parentType
-    })
-  }
-  getPath(config: NewObjectConfig): string {
-    const conf = this.escapedValues(config)
-    return expand(this.pathTemplate, conf) + "/" + conf.name
-  }
-  getBasePath(config: NewObjectConfig): string {
-    return expand(this.pathTemplate, this.escapedValues(config))
-  }
-
-  getValidatePath(config: NewObjectConfig) {
-    return expand(this.validateTemplate, this.escapedValues(config))
-  }
-
-  objNode(newObj: NewObjectConfig): ObjectNode {
-    return {
-      OBJECT_TYPE: this.type,
-      OBJECT_NAME: newObj.name.toLowerCase(),
-      TECH_NAME: newObj.name.toLowerCase(),
-      OBJECT_URI: this.getPath(newObj),
-      OBJECT_VIT_URI: "",
-      EXPANDABLE: ""
-    }
-  }
-
-  getCreatePayload(config: NewObjectConfig) {
-    const payload = `<?xml version="1.0" encoding="UTF-8"?>
-    <${this.rootName} ${this.nameSpace} 
-      xmlns:adtcore="http://www.sap.com/adt/core" 
-      adtcore:description="${config.description}" 
-      adtcore:name="${config.name}" adtcore:type="${this.type}" 
-      adtcore:responsible="${config.responsible}">
-        <adtcore:packageRef adtcore:name="${config.parentName}"/>
-    </${this.rootName}>`
-    return payload
-  }
-}
-
-class FGObjectType extends CreatableObjectType {
-  constructor(
-    public readonly type: string,
-    public readonly label: string,
-    public readonly rootName: string,
-    public readonly nameSpace: string,
-    public readonly pathTemplate: string,
-    public readonly validateTemplate: string
-  ) {
-    super(type, label, rootName, nameSpace, pathTemplate, validateTemplate)
-    this._parentType = "FUGR/F"
-  }
-  getCreatePayload(config: NewObjectConfig) {
-    const payload = `<?xml version="1.0" encoding="UTF-8"?>
-<${this.rootName} ${this.nameSpace} 
-   xmlns:adtcore="http://www.sap.com/adt/core" 
-   adtcore:description="${config.description}" 
-   adtcore:name="${config.name}" adtcore:type="${this.type}" 
-   adtcore:responsible="${config.responsible}"> 
-     <adtcore:containerRef adtcore:name="${config.parentName}" 
-       adtcore:type="${this._parentType}" 
-       adtcore:uri="${this.getBasePath(config)}"/>
-</${this.rootName}>`
-
-    return payload
-  }
-}
-
-export function getObjectType(type: string): CreatableObjectType | undefined {
-  return OBJECTTYPES.find(t => t.type === type)
-}
 export async function selectObjectType(
   parentType?: string
-): Promise<CreatableObjectType | undefined> {
+): Promise<CreatableType | undefined> {
+  const rawtypes = [...CreatableTypes.values()]
   const types = parentType
-    ? OBJECTTYPES.filter(t => t.parentType === parentType)
-    : OBJECTTYPES
-  return window.showQuickPick(types.length > 0 ? types : OBJECTTYPES)
+    ? rawtypes.filter(t => parentTypeId(t.typeId) === parentType)
+    : rawtypes
+  return window.showQuickPick(types || rawtypes)
 }
-
-export const OBJECTTYPES: CreatableObjectType[] = [
-  new CreatableObjectType(
-    "PROG/P",
-    "Program",
-    "program:abapProgram",
-    'xmlns:program="http://www.sap.com/adt/programs/programs"',
-    "/sap/bc/adt/programs/programs",
-    "/sap/bc/adt/programs/validation?objtype={type}&objname={name}&packagename={parentName}&description={description}"
-  ),
-  new CreatableObjectType(
-    "CLAS/OC",
-    "Class",
-    "class:abapClass",
-    'xmlns:class="http://www.sap.com/adt/oo/classes"',
-    "/sap/bc/adt/oo/classes",
-    "/sap/bc/adt/oo/validation/objectname?objtype={type}&objname={name}&packagename={parentName}&description={description}"
-  ),
-  new CreatableObjectType(
-    "INTF/OI",
-    "Interface",
-    "intf:abapInterface",
-    'xmlns:intf="http://www.sap.com/adt/oo/interfaces"',
-    "/sap/bc/adt/oo/interfaces",
-    "/sap/bc/adt/oo/validation/objectname?objtype={type}&objname={name}&packagename={parentName}&description={description}"
-  ),
-  new CreatableObjectType(
-    "FUGR/F",
-    "Function Group",
-    "group:abapFunctionGroup",
-    'xmlns:group="http://www.sap.com/adt/functions/groups"',
-    "/sap/bc/adt/functions/groups",
-    "/sap/bc/adt/functions/validation?objtype={type}&objname={name}&packagename={parentName}&description={description}"
-  ),
-  new FGObjectType(
-    "FUGR/FF",
-    "Function module",
-    "fmodule:abapFunctionModule",
-    'xmlns:fmodule="http://www.sap.com/adt/functions/fmodules"',
-    "/sap/bc/adt/functions/groups/{parentName}/fmodules",
-    "/sap/bc/adt/functions/validation?objtype={type}&objname={name}&fugrname={parentName}&description={description}"
-  ),
-  new CreatableObjectType(
-    "PROG/I",
-    "Include",
-    "include:abapInclude",
-    'xmlns:include="http://www.sap.com/adt/programs/includes"',
-    "/sap/bc/adt/programs/includes",
-    "/sap/bc/adt/includes/validation?objtype={type}&objname={name}&packagename={parentName}&description={description}"
-  ),
-  new FGObjectType(
-    "FUGR/I",
-    "Function group include",
-    "finclude:abapFunctionGroupInclude",
-    'xmlns:finclude="http://www.sap.com/adt/functions/fincludes"',
-    "/sap/bc/adt/functions/groups/{parentName}/includes",
-    "/sap/bc/adt/functions/validation?objtype={type}&objname={name}&fugrname={parentName}&description={description}"
-  )
-]
