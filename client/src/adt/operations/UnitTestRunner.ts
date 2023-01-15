d6dc36018e18d6608a40bb09a10ee7ebfe57bda7
Marcello Urbani <marcello.urbani@gmail.com>
2023-01-15 18:31:18 +0000

    run handler

--- client/src/adt/operations/UnitTestRunner.ts
+++ client/src/adt/operations/UnitTestRunner.ts
@@ -1,4 +1,4 @@
-import { Uri, tests, TestRunProfileKind, TestRunRequest, CancellationToken, TestController, TestItem, TestMessage, MarkdownString, commands, TestItemCollection } from "vscode"
+import { Uri, tests, TestRunProfileKind, TestRunRequest, CancellationToken, TestController, TestItem, TestMessage, MarkdownString, commands, TestItemCollection, TestRun } from "vscode"
 import { alertManagers } from "../../views/abapunit"
 import { getClient, getRoot } from "../conections"
 import { findAbapObject } from "./AdtObjectFinder"
@@ -10,8 +10,13 @@ import { MethodLocator } from "../../views/abapunit/locator"
 import { AbapObject } from "abapobject"
 
 const locators = cache((connId: string) => new MethodLocator(connId))
-const classId = (cl: UnitTestClass) => `${cl["adtcore:type"]} ${cl["adtcore:name"]}`
-const methodId = (meth: UnitTestMethod) => `${meth["adtcore:name"]}`
+const classId = (cl: UnitTestClass) => `C_${cl["adtcore:type"]} ${cl["adtcore:name"]}`
+const methodId = (meth: UnitTestMethod) => `M_${meth["adtcore:name"]}`
+const objectKey = (object: AbapObject) => `O_${object.key}`
+const splitKey = (k: string) => {
+  const [_, key, value] = k.match(/^([CMO])_(.*)/) || []
+  if (key && value) return { key, value }
+}
 
 const mergeTestItems = (coll: TestItemCollection, items: TestItem[]) => {
   const ids = new Set(items.map(i => i.id))
@@ -31,19 +36,7 @@ const getObject = async (uri: Uri) => {
   return findAbapObject(uri)
 }
 
-const alertIcon = (alert: UnitTestAlert) => {
-  switch (alert.kind) {
-    case UnitTestAlertKind.exception:
-      return "testing-error-icon"
-    case UnitTestAlertKind.failedAssertion:
-      return "testing-failed-icon"
-    case UnitTestAlertKind.warning:
-      return "testing-passed-icon"
-  }
-}
-
-
-const convertAlert = (alert: UnitTestAlert): TestMessage => new TestMessage(new MarkdownString(`${alertIcon(alert)} ${alert.details.join("<br>")}`, true))
+const convertAlert = (alert: UnitTestAlert): TestMessage => new TestMessage(new MarkdownString(`${alert.details.join("<br>")}`, true))
 const methodOutcome = (meth: UnitTestMethod) => {
   const passed = !meth.alerts.filter(a => a.kind !== UnitTestAlertKind.warning).length
   return ({ messages: meth.alerts.map(convertAlert), passed })
@@ -56,19 +49,79 @@ export async function abapUnit(uri: Uri) {
   commands.executeCommand("workbench.view.testing.focus")
   await UnitTestRunner.get(uri.authority).addResults(uri, object, testClasses)
 }
-const runHandler = async (
+
+const setMethodResult = (run: TestRun, meth: UnitTestMethod, item: TestItem) => {
+  const outcome = methodOutcome(meth)
+  if (outcome.passed)
+    run.passed(item, meth.executionTime)
+  else
+    run.failed(item, outcome.messages, meth.executionTime)
+}
+
+const setClassResult = (run: TestRun, clas: UnitTestClass, item: TestItem) => {
+  const ids = new Set(clas.testmethods.map(methodId))
+  for (const m of clas.testmethods) {
+    const mi = item.children.get(methodId(m))
+    if (mi) {
+      setMethodResult(run, m, mi)
+    }
+    // TODO
+  }
+  const toRemove = [...item.children].filter(c => !ids.has(c[0]))
+  toRemove.forEach(i => item.children.delete(i[0]))
+}
+
+const setObjectResult = (run: TestRun, classes: UnitTestClass[], item: TestItem) => {
+  const ids = new Set(classes.map(classId))
+  for (const c of classes) {
+    const ci = item.children.get(classId(c))
+    if (ci) {
+      setClassResult(run, c, ci)
+    }
+    // TODO
+  }
+  const toRemove = [...item.children].filter(c => !ids.has(c[0]))
+  toRemove.forEach(i => item.children.delete(i[0]))
+}
+
+const setResults = (run: TestRun, classes: UnitTestClass[], item: TestItem) => {
+  switch (splitKey(item.id)?.key) {
+    case "O":
+      return setObjectResult(run, classes, item)
+    case "C":
+      return setClassResult(run, classes[0], item)
+    case "M":
+      return setMethodResult(run, classes[0].testmethods[0], item)
+  }
+}
+
+
+const runHandler = (runner: UnitTestRunner) => async (
   request: TestRunRequest,
   cancellation: CancellationToken
 ) => {
-  const connId = request.include?.[0].uri?.authority
-  if (!connId) throw new Error("No valid rest in request")
-  const run = UnitTestRunner.get(connId).controller.createTestRun(request)
+  const included = request.include || [...runner.controller.items].map(x => x[1])
+  const connId = included[0].uri?.authority
+  if (!connId) {
+    throw new Error("No valid test found in request")
+  }
+  const run = runner.controller.createTestRun(request, undefined, false)
+  const excluded = (i: TestItem) => request.exclude?.find(ii => i === ii)
   try {
-    const excluded = (i: TestItem) => request.exclude?.find(ii => i === ii)
+    runonTestTree(included, t => run.enqueued(t))
+    runonTestTree(included.filter(x => !excluded(x)), t => run.started(t))
+    runonTestTree(included.filter(excluded), t => run.skipped(t))
+    for (const i of included) {
+      if (excluded(i)) continue
+      const tuPath = runner.aunitPath(i.id)
+      if (!tuPath) throw new Error("Unit test Uri not found")
+      const classes = await getClient(connId).unitTestRun(tuPath)
+      runner.setAuPaths(classes)
+      await setResults(run, classes, i)
+    }
   } finally {
     run.end()
   }
-  log(JSON.stringify(request, undefined, 1))
 }
 
 const runonTestTree = (root: Readonly<TestItem[]>, cb: (i: TestItem) => unknown) => {
@@ -88,11 +141,7 @@ const dummyRunHandler = async (ctrl: TestController, root: TestItemCollection, c
       const methTI = clasTI.children.get(methodId(meth))
       if (!methTI) continue
       run.started(methTI)
-      const outcome = methodOutcome(meth)
-      if (outcome.passed)
-        run.passed(methTI, meth.executionTime)
-      else
-        run.failed(methTI, outcome.messages, meth.executionTime)
+      setMethodResult(run, meth, methTI)
     }
   }
   run.end()
@@ -117,13 +166,12 @@ const createClassItem = async (c: TestController, parent: TestItem, cl: UnitTest
   const item = parent.children.get(classId(cl)) || c.createTestItem(classId(cl), `${cl["adtcore:type"]} ${cl["adtcore:name"]}`, uri)
   const children: TestItem[] = []
   for (const meth of cl.testmethods) children.push(await createMethodItem(c, item, meth, uri, source))
-  // parent.children.add(item)
   mergeTestItems(item.children, children)
   return item
 }
 
 const getOrCreateObjectItem = async (c: TestController, object: AbapObject, classes: UnitTestClass[], uri: Uri) => {
-  const item = c.items.get(object.key) || c.createTestItem(object.key, object.key, uri)
+  const item = c.items.get(objectKey(object)) || c.createTestItem(objectKey(object), object.key, uri)
   const children: TestItem[] = []
   for (const cl of classes) children.push(await createClassItem(c, item, cl, uri))
   c.items.add(item)
@@ -134,6 +182,10 @@ const getOrCreateObjectItem = async (c: TestController, object: AbapObject, clas
 export class UnitTestRunner {
   private static instances = new Map<string, UnitTestRunner>()
   readonly controller
+  private aunitPaths = new Map<string, string>()
+  aunitPath(key: string) {
+    return this.aunitPaths.get(key)
+  }
   static get(connId: string) {
     const old = this.instances.get(connId)
     if (old) return old
@@ -141,13 +193,24 @@ export class UnitTestRunner {
     this.instances.set(connId, instance)
     return instance
   }
+
   private constructor(readonly connId: string) {
     this.controller = tests.createTestController(`ABAP${connId}`, `ABAP ${connId}`)
-    this.controller.createRunProfile(`ABAP ${connId}`, TestRunProfileKind.Run, runHandler, true)
+    this.controller.createRunProfile(`ABAP ${connId}`, TestRunProfileKind.Run, runHandler(this), true)
+  }
+
+  setAuPaths(classes: UnitTestClass[]) {
+    for (const c of classes) {
+      this.aunitPaths.set(classId(c), c["adtcore:uri"])
+      for (const m of c.testmethods)
+        this.aunitPaths.set(methodId(m), m.navigationUri || m["adtcore:uri"])
+    }
   }
 
   async addResults(uri: Uri, object: AbapObject, classes: UnitTestClass[]) {
     const root = await getOrCreateObjectItem(this.controller, object, classes, uri)
+    this.aunitPaths.set(objectKey(object), object.path)
+    this.setAuPaths(classes)
     await dummyRunHandler(this.controller, root.children, classes)
   }
 }
