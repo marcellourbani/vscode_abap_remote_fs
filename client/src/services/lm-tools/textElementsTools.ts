ef8dbb2f92cf50a911f3eb3887435bdb1ddf8b6f
aravindhkce <aravindhkce@gmail.com>
2026-07-17 16:51:17 +0530

    rename

--- client/src/services/lm-tools/textElementsTools.ts
+++ client/src/services/lm-tools/textElementsTools.ts
@@ -94,9 +94,9 @@ export class ManageTextElementsTool implements vscode.LanguageModelTool<IManageT
         actualConnectionId = activeEditor.document.uri.authority
       }
 
-      const client_type = action === "read" ? true : false
+      const stateless_clone = action === "read" ? true : false
       // Get connection - stateless clone for read and stateful client for create/update
-      const client = getClient(actualConnectionId, client_type)
+      const client = getClient(actualConnectionId, stateless_clone)
       if (!client) {
         throw new Error("No ADT connection available. Please connect to an SAP system first.")
       }
